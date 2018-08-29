72c7382e4d2691c1225e657e331ad07ea7e544b7
Alexis Muñoz <alexismun53@gmail.com>
2018-08-28 22:53:11 -0500

    add navbar functionality

--- src/App.js
+++ src/App.js
@@ -34,7 +34,8 @@ class App extends Component {
       input: '',
       imageUrl: '',
       box: {},
-      route: 'signin'
+      route: 'signin',
+      isSignedIn: false
     }
   }
 
@@ -68,19 +69,28 @@ class App extends Component {
   }
 
   onRouteChange = (route) => {
+  	if (route === 'signout') {
+  		this.setState({ isSignedIn: false });
+  	} else if ( route === 'home') {
+  		this.setState({ isSignedIn: true });
+  	}
   	this.setState({route});
   }
 
   render() {
+  	const { isSignedIn, route, box, imageUrl } = this.state;
     return (
       <div className="App">
         <Particles 
           className='particles' 
           params={particlesOptions}
         />
-        <Navigation onRouteChange={ this.onRouteChange } />
+        <Navigation 
+        	onRouteChange={ this.onRouteChange } 
+        	isSignedIn={ isSignedIn } 
+        />
         {
-        	this.state.route === 'home'
+        	route === 'home'
         	? <div>
 		    	<Logo />
 			    <Rank />
@@ -89,12 +99,12 @@ class App extends Component {
 			      onButtonSubmit = { this.onButtonSubmit }
 			    />
 			    <FaceRecognition 
-			      box = { this.state.box }
-			      imageUrl = { this.state.imageUrl }
+			      box = { box }
+			      imageUrl = { imageUrl }
 			    />
 			 </div>
 		    : (
-		    	this.state.route === 'signin'
+		    	route === 'signin'
 		    	? <Signin onRouteChange={ this.onRouteChange } />
 		    	: <Register onRouteChange={ this.onRouteChange } />
 		    )
